27ac76bc07f43e4e10c6e13be57e14fbce0458e1
izzatuna <izzhanifa@gmail.com>
2021-02-24 15:32:49 +0700

    belajar php dasar

---
+++ dasar.php
@@ -0,0 +1,26 @@
+<?php
+
+    define("PHI",3.14);
+    $jari_jari = 4;
+    $luas_lingkarang = PHI * $jari_jari * $jari_jari;
+    echo $luas_lingkarang;
+    echo "</br>";
+
+
+    $nama = "hani";
+    $angka = 1;
+    $angka_desimal = 2.5;
+    echo $angka;
+    echo "</br>";
+    echo $nama;
+    echo "</br>";
+    echo $angka_desimal;
+
+    echo "</br>";
+    echo "dokumen saya : ".$_SERVER["DOCUMENT_ROOT"];
+
+    echo "</br>";
+    echo "nama saya"; .$nama;
+
+
+?>
